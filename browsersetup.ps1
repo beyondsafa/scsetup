98acc5efe0f071719eda6dbf172a393b8bbaca37
beyondsafa <beyondsafa@gmail.com>
2026-04-12 12:15:10 +0600

    Fix environment variable for Scoop directory
    Updated environment variable check for Scoop directory.

--- browsersetup.ps1
+++ browsersetup.ps1
@@ -1,20 +1,7 @@
-# Script for Browser Setup
-
-# This script sets up browsers for use with custom Scoop directory locations.
-
-# Define environment variable for custom Scoop directory if set
+# Current (incorrect):
 if ($env:SCOOP_ROOT -ne $null) {
     $scoopRoot = $env:SCOOP_ROOT
-} else {
-    $scoopRoot = "$HOME\scoop"
-}
 
-# Path to the browsers installation in the Scoop directory
-$browsersPath = "$scoopRoot\shims"
-
-# Install or set up browsers with Scoop
-scoop install firefox
-scoop install chrome
-scoop install edge
-
-Write-Host "Browsers have been set up in: $browsersPath"
+# Should be:
+if ($env:SCOOP -ne $null) {
+    $scoopDir = $env:SCOOP
